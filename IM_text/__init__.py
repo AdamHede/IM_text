7ef74111c53e62e1b22fd07418a80015e88b085d
AdamHede <adhe@implement.dk>
2020-05-08 11:39:45 +0200

    basic cleaner function

--- IM_text/__init__.py
+++ IM_text/__init__.py
@@ -1,25 +1,20 @@
 from string import punctuation, digits
+from ._stopwords import DANISH_STOP_WORDS, ENGLISH_STOP_WORDS
 
 digits_and_punctuation = punctuation + digits
-
-with open("IM_text/stopwords_da.txt", "r") as file:
-    stopwords_da = file.read().splitlines()
-
-with open("IM_text/stopwords_en.txt", "r") as file:
-    stopwords_en = file.read().splitlines()
 
 def clean_string(text, lower_case = True, remove='digits and punctuation', stopwords='danish'):
     if lower_case:
         text = text.lower()
-    if remove is 'digits and punctuation':
+    if remove == 'digits and punctuation':
         remove = digits_and_punctuation
     if remove is not None:
         text = text.translate(str.maketrans(remove, " " * len(remove)))
     if stopwords is not None:
         if stopwords is 'danish':
-            stopwords = stopwords_da
+            stopwords = DANISH_STOP_WORDS
         elif stopwords is 'english':
-            stopwords = stopwords_en
+            stopwords = ENGLISH_STOP_WORDS
         else:
             stopwords = stopwords
         text = [word for word in text.split() if word not in stopwords]

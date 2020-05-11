2ded12d25123cdfb2b8e49d69035e6e84ad7b8ff
AdamHede <adhe@implement.dk>
2020-05-11 13:20:07 +0200

    New Documentation

--- doc/conf.py
+++ doc/conf.py
@@ -42,6 +42,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# MANUALLY ADDDED ACCORDING TO https://github.com/readthedocs/readthedocs.org/issues/2569
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
